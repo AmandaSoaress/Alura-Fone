d1b30df6ee90133cb919f46360d79dd76dab522e
Amanda Santos <113608404+AmandaSoaress@users.noreply.github.com>
2023-04-17 11:41:50 -0300

    js

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ function aperte(value){
    alert(value);
 }
 
+var teclado = document.querySelector("section");
+teclado.addEventListener('click', item); 
+
+function item(evento){
+   var input = evento.target
+  console.log(input.value);
+}
